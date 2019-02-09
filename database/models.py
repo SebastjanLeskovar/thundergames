3570ad1452409107ae245c75326c5604434e1070
Sebastjan Leskovar <sebastjan.leskovar@gmail.com>
2019-02-09 22:33:52 +0100

    V2.5

--- database/models.py
+++ database/models.py
@@ -11,7 +11,7 @@ class TimeStampedModel(models.Model):
 class MainGenre(TimeStampedModel):
     """Model to create MainGenre objects."""
     name = models.CharField("Name", max_length=50)
-    image = models.ImageField("Image", upload_to="media/main_genre/", blank=True)
+    image = models.ImageField("Image", upload_to='main_genre', blank=True)
 
     def __str__(self):
         return self.name
@@ -23,7 +23,7 @@ class SubGenre(TimeStampedModel):
     """Model to create SubGenre objects."""
     name = models.CharField("Name", max_length=50)
     main_genre = models.ForeignKey(MainGenre, verbose_name="main genre", on_delete=models.SET("Please select main genre."))
-    image = models.ImageField("Image", upload_to="media/subgenre/", blank=True)
+    image = models.ImageField("Image", upload_to='subgenre', blank=True)
 
     def __str__(self):
         return self.name
@@ -37,7 +37,7 @@ class Game(TimeStampedModel):
     name = models.CharField("Name", max_length=50)
     main_genre = models.ForeignKey(MainGenre, verbose_name="main genre", blank=True, on_delete=models.SET("Please select main genre."))
     subgenres = models.ManyToManyField(SubGenre, verbose_name="Subgenre", blank=True)
-    image = models.ImageField("Image", upload_to='media/game/', blank=True)
+    image = models.ImageField("Image", upload_to='game', blank=True)
 
     def __str__(self):
         return self.name
